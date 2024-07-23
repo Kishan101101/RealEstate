7b87e41d5af36dbaf0ed318a5198b105bc436412
Kishan Yadav <guls.yadav@gmail.com>
2024-07-23 13:28:10 +0530

    connect to database

--- API/index.js
+++ API/index.js
@@ -1,5 +1,22 @@
 import express from "express";
-const app = express();
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+// URL-encode your password if it contains special characters
+
+const uri = process.env.MONGO_URI;
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    const app = express();
+
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
